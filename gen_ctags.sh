b109cf1d845c87d79cba8ca0285fd41b4e251653
Ricardo Gaviria <ricardo.a.gaviria@gmail.com>
2017-04-29 01:07:05 +0100

    Updating gen_ctags.sh but cant remember what was the reason + seem to be specifying current working directory explicitly rather than using .

--- gen_ctags.sh
+++ gen_ctags.sh
@@ -1,1 +1,1 @@
-ctags -R -f .git/tags --python-kinds=-i --exclude=.git --exclude=log . $(python -c "import os, sys; print(' '.join('{}'.format(d) for d in sys.path if os.path.isdir(d)))")
+ctags -R -f .git/tags --python-kinds=-iv --exclude=.git --exclude=log $(pwd) $(python -c "import os, sys; print(' '.join('{}'.format(d) for d in sys.path if os.path.isdir(d)))")
